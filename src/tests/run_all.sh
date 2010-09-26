c272507b4eab2a234d05581ceb83794d0e61e913
Gian Perrone <gian.perrone@gmail.com>
2010-09-26 04:29:10 +0200

    Added additional tests for KuPEG

--- src/tests/run_all.sh
+++ src/tests/run_all.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
+echo "KIL tests:"
 for i in *.k
 do
 	printf "%25s: " "$i"
 	../../bin/kil -v 3 $i > $i.out
 	OUT=`md5sum $i.out`
 	EXP=`grep "$i.out" expected.dat`
+	if [ "$OUT" != "$EXP" ]; then
+		echo "	[FAILED]"
+	else echo "	[passed]"
+	fi
+done
+
+echo "KuPEG tests:"
+for i in *.kpg
+do
+	printf "%25s: " "$i"
+	../../bin/kupeg $i
+   mv "$i.k" "$i.k.out"
+   OUT=`md5sum $i.k.out`
+	EXP=`grep "$i.k.out" expected.dat`
 	if [ "$OUT" != "$EXP" ]; then
 		echo "	[FAILED]"
 	else echo "	[passed]"
