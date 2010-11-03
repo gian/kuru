2f2ccce80738d4671a524b7a5d9ce0f5d5b88ee2
Gian Perrone <gian.perrone@gmail.com>
2010-11-03 22:58:43 +1300

    Improved code generation for datatype declarations and pattern matching of constructors.

--- src/runtime/kuru_types.h
+++ src/runtime/kuru_types.h
@@ -3,6 +3,11 @@ typedef struct kuru_string_t {
   char *data;
 } kuru_string_t;
 
+typedef struct kuru_list_t {
+	char *data;
+	struct kuru_list_t *next;
+} kuru_list_t;
+
 kuru_string_t *c2ks(const char *cstr);
 void kuru_string_destroy(kuru_string_t *kstr);
 kuru_string_t *ksdup(const kuru_string_t *kstr);
