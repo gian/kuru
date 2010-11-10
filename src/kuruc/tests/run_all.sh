e6509f6a462f796dba1b715eb6baf32aa9b17a46
Gian Perrone <gian.perrone@gmail.com>
2010-11-10 16:41:45 +1300

    Fixed kuruc test run script and debug reporting so -qq switch is obeyed.

--- src/kuruc/tests/run_all.sh
+++ src/kuruc/tests/run_all.sh
@@ -3,13 +3,17 @@
 echo "Kuruc tests:"
 for i in *.k
 do
-	printf "%28s: " "$i"
-	../../../bin/kuruc $i > $i.out
-	OUT=`md5sum $i.out`
-	EXP=`grep "$i.out" expected.dat`
-	if [ "$OUT" != "$EXP" ]; then
+	printf "%35s: " "$i"
+	../../../bin/kuruc -qq -L ../../runtime:../../lib:../../../lib ../../lib/kuru.kb $i -o $i.out 2> /dev/null
+	if [ $? -eq 0 ] ; then
+		OUT=`md5sum $i.out`
+		EXP=`grep "$i.out" expected.dat`
+		if [ "$OUT" != "$EXP" ]; then
+			echo "	[FAILED]"
+		else echo "	[passed]"
+		fi
+	else
 		echo "	[FAILED]"
-	else echo "	[passed]"
 	fi
 done
 
