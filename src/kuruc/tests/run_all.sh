861afcbd9e032dbe377350d833784d5aaec230ab
Gian Perrone <gian.perrone@gmail.com>
2010-11-14 15:36:29 +1300

    Fixed some minor bugs that were breaking some tests.

--- src/kuruc/tests/run_all.sh
+++ src/kuruc/tests/run_all.sh
@@ -2,23 +2,32 @@
 
 export KURU_LIBRARY_PATH=../../runtime:../../lib:../../../lib
 
+f=0
+t=0
+
 echo "Kuruc tests:"
 for i in *.k
 do
+	t=$(($t+1))
 	printf "%35s: " "$i"
-	../../../bin/kuruc -qq $i -o $i.out 2> /dev/null
+	../../../bin/kuruc -q $i -o $i.out 2> $i.log
 #	../../../bin/kuruc $i -o $i.out -d
 	if [ $? -eq 0 ] ; then
 		OUT=`md5sum $i.out`
 		EXP=`grep "$i.out" expected.dat`
 		if [ "$OUT" != "$EXP" ]; then
 			echo "	[FAILED (Compiled Output Mismatch)]"
+			f=$(($f+1))
 		else echo "	[passed]"
 		fi
 	else
+		f=$(($f+1))
 		echo "	[FAILED (Compilation Failed)]"
 	fi
 done
+
+echo ""
+echo " * Summary: $(($t-$f))/$t tests passed.  $f failures."
 
 
 
